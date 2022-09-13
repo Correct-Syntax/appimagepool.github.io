1906a4979d32a4bc4adb9bed8920bb0fdb790283
I Woithe <iwoithe@just42.net>
2022-09-14 08:03:38 +0930

    Small update

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,23 +2,43 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 function NavigationBar(props: any) {
-  return <nav className="w-full py-8 h-112px">
-    <div className="absolute w-full">
-      <div className="flex flex-row justify-between w-full place-items-center">
-        <div className="ml-50px">
-          <Link href="/">
-            <Image src="/appimagepool.svg" width={48} height={48} />
-          </Link>
+  if (props.absolute === true) {
+    return (
+      <nav className="absolute w-full py-8 h-112px">
+        <div className="flex flex-row justify-between w-full place-items-center">
+          <div className="ml-50px">
+            <Link href="/">
+              <Image src="/appimagepool.svg" width={48} height={48} />
+            </Link>
+          </div>
+          <div className="mr-50px">
+            <Link href="/"><a className="px-4 font-bold text-accent">Home</a></Link>
+            <Link href="/#blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
+            <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
+            <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
+          </div>
         </div>
-        <div className="mr-50px">
-          <Link href="/"><a className="px-4 font-bold text-accent">Home</a></Link>
-          <Link href="/#blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
-          <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
-          <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
+      </nav>
+    )
+  } else {
+    return (
+      <nav className="w-full py-8 h-112px">
+        <div className="flex flex-row justify-between w-full place-items-center">
+          <div className="ml-50px">
+            <Link href="/">
+              <Image src="/appimagepool.svg" width={48} height={48} />
+            </Link>
+          </div>
+          <div className="mr-50px">
+            <Link href="/"><a className="px-4 font-bold text-accent">Home</a></Link>
+            <Link href="/#blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
+            <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
+            <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>;
+      </nav>
+    )
+  }
 }
 
 export default NavigationBar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,32 @@ import type { AppProps } from 'next/app'
 import AIPHead from '../components/Head'
 import Footer from '../components/Footer'
 import NavigationBar from '../components/NavigationBar'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // TODO: Make a responsive design
-  return <div>
-    <AIPHead />
-    <NavigationBar currentPage="Home" />
-    <Component {...pageProps} />
-    <Footer />
-  </div>
+  const router = useRouter()
+  console.log(router.pathname)
+  
+  if (router.pathname === "/") {
+    return (
+      <div>
+        <AIPHead />
+        <NavigationBar absolute={ true } currentPage="Home" />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <AIPHead />
+        <NavigationBar absolute={ false } currentPage="Home" />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default MyApp
