dd88d8f5865ab01f431cf3144224c85f87062d53
Noah Rahm <correctsyntax@yahoo.com>
2023-01-10 17:03:09 -0600

    Make navigation mobile responsive

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,43 +2,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 function NavigationBar(props: any) {
-  if (props.absolute === true) {
-    return (
-      <nav className="absolute w-full py-8">
-        {/* <div className="flex flex-row justify-between w-full place-items-center">
-          <div className="ml-50px">
-            <Link href="/">
-              <Image src="/appimagepool.svg" width={48} height={48} />
-            </Link>
-          </div>
-          <div className="mr-50px">
-            <Link href="/"><a className="px-4 font-bold text-accent">Home</a></Link>
-            <Link href="/blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
-            <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
-            <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
-          </div>
-        </div> */}
-      </nav>
-    )
-  } else {
-    return (
-      <nav className="w-full py-8 h-112px">
-        <div className="flex flex-row justify-between w-full place-items-center">
-          <div className="ml-50px">
-            <Link href="/">
-              <Image src="/appimagepool.svg" width={48} height={48} />
-            </Link>
-          </div>
-          <div className="mr-50px">
-            <Link href="/"><a className="px-4 font-bold text-accent">Home</a></Link>
-            <Link href="/#blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
-            <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
-            <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
-          </div>
+  return (
+    <nav className="relative w-full max-w-screen-xl py-8 mx-auto px-10">
+      <div className="flex flex-col md:flex-row justify-between w-full place-items-center">
+        <div className="cursor-pointer">
+          <Link href="/">
+            <Image src="/appimagepool.svg" width={48} height={48} />
+          </Link>
         </div>
-      </nav>
-    )
-  }
+        <div className="mt-6 md:mt-0">
+          <Link href="/"><a className="px-4 font-bold text-accent hidden md:inline">Home</a></Link>
+          <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
+          <Link href="/blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
+          <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default NavigationBar
