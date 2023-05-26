f44ba0d62eaf6be482ad6292dd17f6db0d7a0bcd
Noah Rahm <correctsyntax@yahoo.com>
2023-05-26 14:14:50 -0500

    Fix the next.js refs error the right way * The not immediately obvious right way

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,18 @@
+import Link from 'next/link'
 
 function Button(props: any) {
     const linkPath: string = props.link
 
     if (props.isAccented == true) {
-        return <a href={linkPath} className="bg-accent text-white w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer hover:bg-secondary hover:text-accent ease-in-out duration-300">{ props.text }</a>;
+        return <div><Link href={{ pathname: linkPath }} scroll={false}><a className="bg-accent text-white w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer hover:bg-secondary hover:text-accent ease-in-out duration-300">{ props.text }</a></Link></div>;
     }
-    
+
     if (props.isFlat == true) {
-        return <a href={ linkPath } className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300" >{ props.text }</a>;
+        return <div><Link href={{ pathname: linkPath }} scroll={false}><a className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300" >{ props.text }</a></Link></div>;
     }
-    
-    return <a href={  linkPath } className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300">{ props.text }</a>;
+
+    return <div><Link href={{ pathname: linkPath }} scroll={false} passHref><a className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300">{ props.text }</a></Link></div>;
 }
+        
 
 export default Button
--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 function NavigationBar(props: any) {
   return (
     <nav className="relative w-full max-w-screen-xl py-8 mx-auto px-10">
       <div className="flex flex-col md:flex-row justify-between w-full place-items-center">
         <div className="cursor-pointer">
-          <a href="/">
+          <Link href="/">
             <Image src="/appimagepool.svg" width={48} height={48} />
-          </a>
+            
+          </Link>
         </div>
         <div className="mt-6 md:mt-0">
-          <a href="/" className="px-4 font-bold text-accent hidden md:inline">Home</a>
-          <a href="/#features" className="px-4 font-bold text-accent">Features</a>
-          <a href="/blog" className="px-4 font-bold text-accent">Blog</a>
-          <a href="/#download" className="px-4 font-bold text-accent">Download</a>
+          <Link href="/"><a className="px-4 font-bold text-accent hidden md:inline">Home</a></Link>
+          <Link href="/#features"><a className="px-4 font-bold text-accent">Features</a></Link>
+          <Link href="/blog"><a className="px-4 font-bold text-accent">Blog</a></Link>
+          <Link href="/#download"><a className="px-4 font-bold text-accent">Download</a></Link>
         </div>
       </div>
     </nav>
