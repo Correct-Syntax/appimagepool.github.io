fe3c4dea2a34c20436cd3829f38312f627d48d7e
Noah Rahm <correctsyntax@yahoo.com>
2023-05-26 13:54:35 -0500

    Fix next.js refs error * * This would be my solution. - I can't tell  why using Link would be necessary so this commit reverts to using an anchor tag

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link'
 
 function Button(props: any) {
     const linkPath: string = props.link
 
     if (props.isAccented == true) {
-        return <Link href={{ pathname: linkPath }} scroll={false}><a className="bg-accent text-white w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer hover:bg-secondary hover:text-accent ease-in-out duration-300">{ props.text }</a></Link>;
+        return <a href={linkPath} className="bg-accent text-white w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer hover:bg-secondary hover:text-accent ease-in-out duration-300">{ props.text }</a>;
     }
     
     if (props.isFlat == true) {
-        return <Link href={{ pathname: linkPath }} scroll={false}><a className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300" >{ props.text }</a></Link>;
+        return <a href={ linkPath } className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300" >{ props.text }</a>;
     }
     
-    return <Link href={{ pathname: linkPath }} scroll={false}><a className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300">{ props.text }</a></Link>;
+    return <a href={  linkPath } className="text-accent hover:bg-secondary w-180px h-14 rounded-md flex flex-row justify-center place-items-center text-center cursor-pointer ease-in-out duration-300">{ props.text }</a>;
 }
 
 export default Button
